f48105ba3a696f79772504b22141b19001e3afae
ishidaybi <130613838+ishidaybi@users.noreply.github.com>
2024-04-29 21:05:16 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
-let nome = "felipe";
-console.log("oi");
+let heroiNome = "Kurama";
+let heroiXp = 100;
+
+
+if (heroiXp <= 1000) {
+    message = "Ferro"
+} else if (heroiXp <=2000){
+    message = "Bronze"
+} else if (heroiXp <=5000){
+    message = "Prata"
+} else if (heroiXp <=7000){
+    message = "Ouro"
+} else if (heroiXp <=8000){
+    message = "Platina"
+} else if (heroiXp <=9000){
+    message = "Ascendente"
+} else if (heroiXp <=10000){
+    message = "Imortal"
+} else if (heroiXp > 10000){
+    message = "Radiante"
+} 
+
+console.log("O Herói " + heroiNome + " está no nível " + message)
